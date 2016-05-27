328556c52341a0c46f98086e1996cc8cd90caf0d
Oscar Zarta <ozarta2093@gmail.com>
2016-05-27 14:41:16 -0500

    primer comentario

---
+++ stylesheet/logica.js
@@ -0,0 +1,36 @@
+var app = angular.module('softdesk',['ui.router']);
+
+app.directive('barranav',function () {
+    return{
+        restrict: 'E',
+        templateUrl: 'barraleft.html'
+    }
+})
+
+app.directive('barrabusqueda',function () {
+    return{
+        restrict: 'A',
+        templateUrl: 'barratop.html'
+    }
+})
+
+app.config(['$stateProvider','$urlRouterProvider',function (stateProvider,urlRouterProvider) {
+    stateProvider
+        .state('nradicado',{
+            url: '/nradicado',
+            template: '<h1 style="margin: 0">Componente prueba</h1>'
+
+        })
+        .state('zz',{
+            url: '/zz',
+            template: '<h1 style="margin: 0">Componente 3</h1>'
+
+        })
+
+        .state('stiker',{
+            url: '/stiker',
+            templateUrl: 'stiker.html'
+
+        })
+}]);
+
